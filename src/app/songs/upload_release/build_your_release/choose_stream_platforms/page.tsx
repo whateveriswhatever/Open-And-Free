9720508409b6ebe3cc48bda13c4aeadf73c4e3e9
Toru Dick <banvietthenaocungdc@gmail.com>
2024-07-06 17:22:49 +0700

    Constructing page for picking song's stream platform.

---
+++ src/app/songs/upload_release/build_your_release/choose_stream_platforms/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import HeaderNav from '../../{components}/Header-Nav';
+
+const page = () => {
+  return (
+    <div id="pick-up-stream-platform"
+    className="bg-amber-50 text-stone-500 desktop:font-[2rem]">
+      <>
+        <HeaderNav context="Let's build you release" />
+      </>
+
+      <>
+      </>
+    </div>
+  )
+}
+
+export default page
